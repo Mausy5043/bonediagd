1d95b2904be6029a03f16166082a81a746993a04
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-13 14:32:49 +0100

    20151213T1432

--- testAIN.py
+++ testAIN.py
@@ -4,7 +4,7 @@ import time
 
 sensor_pin = 'AIN6'
 # sensor calibration data
-tmp36_mV2deg =
+#tmp36_mV2deg =
 tmp36_gain = 1.0
 tmp36_offset = 0.0
 
@@ -12,13 +12,14 @@ ADC.setup()
 
 while True:
     reading = ADC.read(sensor_pin)
-    millivolts = reading * 1800  # reference = 1800 mV
-    temp_c = (millivolts - 500) / 10
+    millivolts = reading * 1800.0  # reference = 1800 mV
+    temp_c = (millivolts - 500) / 10.0
     temp_c = temp_c * tmp36_gain + tmp36_offset
 
     raw_millivolt = ADC.read_raw(sensor_pin)
-    temp_cr = (raw_millivolt - 500) / 10
-    print('mv=%d C=%.2f rw=%d F=%.2f' % (millivolts, temp_c, raw_millivolt, temp_cr))
+    temp_cr = (raw_millivolt - 500) / 10.0
+    temp_cr = temp_cr * tmp36_gain + tmp36_offset
+    print('mv=%d C=%.2f rw=%d Cr=%.2f' % (millivolts, temp_c, raw_millivolt, temp_cr))
     time.sleep(30)
 
 # r
