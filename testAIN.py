4553e6ad3365cef1d0dbc12c3c8e1b8cccea1838
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-13 14:12:49 +0100

    add calibration support

--- testAIN.py
+++ testAIN.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python
 import Adafruit_BBIO.ADC as ADC
 import time
 
 sensor_pin = 'AIN6'
+# sensor calibration data
+tmp36_gain = 1.0
+tmp36_offset = 0.0
 
 ADC.setup()
 
 while True:
     reading = ADC.read(sensor_pin)
-    raw_reading = ADC.read_raw(sensor_pin)
+    raw_reading = ADC.raw_read(sensor_pin)
     millivolts = reading * 1800  # reference = 1800 mV
     temp_c = (millivolts - 500) / 10
+    temp_c = temp_c * tmp36_gain + tmp36_offset
     temp_f = (temp_c * 9/5) + 32
-    print('rw=%d mv=%d C=%d F=%d' % (raw_reading, millivolts, temp_c, temp_f))
+    print('rw=%d mv=%d C=%.2f F=%d' % (raw_reading, millivolts, temp_c, temp_f))
     time.sleep(1)
