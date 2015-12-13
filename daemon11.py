74138a421d5ee9ce741bbdbb8ad436f3116804fb
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-13 15:44:36 +0100

    see if this works

--- daemon11.py
+++ daemon11.py
@@ -92,7 +92,7 @@ def do_work():
 
 def do_report(result):
   # Get the time and date in human-readable form and UN*X-epoch...
-  outDate = commands.getoutput("date '+%F %H:%M:%S, %s'")
+  outDate = commands.getoutput("date '+%FT%H:%M:%S, %s'")
   flock = '/tmp/bonediagd/11.lock'
   lock(flock)
   f = file('/tmp/bonediagd/11-t-cpu.csv', 'a')
