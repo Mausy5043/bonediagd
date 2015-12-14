71dd1ed4b64e3a56a496bb51893d947a8a76d671
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-14 18:44:15 +0100

    cleanup

--- testAIN.py
+++ testAIN.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import Adafruit_BBIO.ADC as ADC
-import time, commands, os
+import time, os
 
 sensor_pin = 'AIN6'
 # SENSOR CALIBRATION PROCEDURE
@@ -15,19 +15,19 @@ sensor_pin = 'AIN6'
 #   found for gain(new) and offset(new)
 
 # gain(old)
-tmp36_gain = 1.0
+TMP36_gain = 1.0
 # offset(old)
-tmp36_offset = 0.0
+TMP36_offset = 0.0
 
 ADC.setup()
 
 def mV2degC(millivolt):
-  degC = (degC * tmp36_gain) + tmp36_offset
+  degC = (degC * TMP36_gain) + TMP36_offset
   return degC
 
 def do_report(result):
   # Get the time and date in human-readable form and UN*X-epoch...
-  outDate = commands.getoutput("date '+%FT%H:%M:%S, %s'")
+  outDate = time.strftime('%Y-%m-%dT%H:%M:%S, %s')
   flock = '/tmp/TMP36.lock'
   lock(flock)
   f = file('/tmp/TMP36.csv', 'a')
@@ -46,16 +46,7 @@ def unlock(fname):
 if __name__ == "__main__":
 
   while True:
-    #reading = ADC.read(sensor_pin)
-    #millivolts = reading * 1800.0  # reference = 1800 mV
-    #temp_c = mV2degC(millivolts)
-    #temp_c = (millivolts - 500) / 10.0
-    #temp_c = temp_c * tmp36_gain + tmp36_offset
-
-    raw_millivolt = ADC.read_raw(sensor_pin)
-    temp_cr = mV2degC(raw_millivolt)
-    #temp_cr = (raw_millivolt - 500) / 10.0
-    #temp_cr = temp_cr * tmp36_gain + tmp36_offset
-    #print('mv=%d C=%.2f rw=%d Cr=%.2f' % (millivolts, temp_c, raw_millivolt, temp_cr))
-    do_report(temp_cr)
+    rawMillivolt = ADC.read_raw(sensor_pin)
+    temperature = mV2degC(rawMillivolt)
+    do_report(temperature)
     time.sleep(60)
