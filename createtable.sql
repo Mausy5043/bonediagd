b993c9d023ca994cb5c4002727c3ed2e438bc852
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-20 17:55:27 +0100

    sql script

--- createtable.sql
+++ createtable.sql
@@ -1,24 +1,13 @@
 # Below are the commands required to recreate (without data) the table for DHCPNS
 
 USE domotica;
-# mysql> SHOW TABLES;
-# +--------------------+
-# | Tables_in_dhcpnsdb |
-# +--------------------+
-# | lantbl             |
-# +--------------------+
-# 1 row in set (0.00 sec)
 
-# +--------+
-# | Table  |
-# +--------+
-# | lantbl |
-# +--------+
+DROP TABLE IF EXISTS tmp36;
 
 CREATE TABLE `tmp36` (
-  `sample_time` datetime DEFAULT NOT NULL,
-  `sample_epoch` int(11) NOT NULL,
-  `raw_value` decimal(6,2) NULL,
-  `temperature` decimal(5,2) DEFAULT NULL,
+  `sample_time`  datetime,
+  `sample_epoch` int(11),
+  `raw_value`    decimal(6,2),
+  `temperature`  decimal(5,2),
   PRIMARY KEY (`sample_time`)
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
