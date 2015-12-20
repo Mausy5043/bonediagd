3191794c8160af1ac23de8f9a0f0addd860c7a3e
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-20 18:00:26 +0100

    epoch is unsigned

--- createtable.sql
+++ createtable.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS tmp36;
 
 CREATE TABLE `tmp36` (
   `sample_time`  datetime,
-  `sample_epoch` int(11),
+  `sample_epoch` int(11) unsigned,
   `raw_value`    decimal(6,2),
   `temperature`  decimal(5,2),
   PRIMARY KEY (`sample_time`)
