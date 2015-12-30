8119a3fd9f8c59457c930077956d342ce0e95117
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-30 18:46:04 +0100

    corrected scriptname

--- testgraph.sh
+++ testgraph.sh
@@ -2,5 +2,5 @@
 
 pushd $HOME/bonediagd
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM tmp36 where (sample_time) >=NOW() - INTERVAL 6 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql.csv
-  testgraph.gnu
+  ./testgraph.gp
 popd
