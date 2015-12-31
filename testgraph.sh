317856f0425f78307c241e0c0c1320a345389c21
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-31 16:34:55 +0100

    testing variable pass-thru

--- testgraph.sh
+++ testgraph.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+LOCAL=$(date)
+LOCALSECONDS=$(date -d "$LOCAL" +%s)
+UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
+UTCSECONDS=$(date -d "$UTC" +%s)
+TIMEZONEGAP=$(($LOCALSECONDS-$UTCSECONDS))
+
 pushd $HOME/bonediagd
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM tmp36 where (sample_time) >=NOW() - INTERVAL 6 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql.csv
   ./testgraph.gp
