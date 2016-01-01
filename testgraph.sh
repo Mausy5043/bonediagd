643549be670c71fa9bd6f98a27b48a339fe2fc0a
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-01-01 11:55:13 +0100

    scriptname changed

--- testgraph.sh
+++ testgraph.sh
@@ -8,5 +8,5 @@ TIMEZONEGAP=$(($LOCALSECONDS-$UTCSECONDS))
 
 pushd $HOME/bonediagd
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM tmp36 where (sample_time) >=NOW() - INTERVAL 6 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql.csv
-  ./testgraph.gp
+  ./graph21.gp
 popd
