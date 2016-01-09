073a71c12e8b7f33b707814fe7ccd1c124df3d64
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-01-09 13:24:02 +0100

    rename file

--- table21.sql
+++ table21.sql
@@ -13,5 +13,5 @@ CREATE TABLE `tmp36` (
   PRIMARY KEY (`sample_time`)
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
 
-# retrieve data:
+# example to retrieve data:
 -- mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM tmp36 where (sample_time) >=NOW() - INTERVAL 6 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql.csv
