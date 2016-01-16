017854b2a0b1178fffa2f47cc6bbd32d32e98265
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-01-16 13:15:23 +0100

    1 day

--- testgraph.sh
+++ testgraph.sh
@@ -7,11 +7,11 @@ UTCSECONDS=$(date -d "$UTC" +%s)
 UTCOFFSET=$(($LOCALSECONDS-$UTCSECONDS))
 
 pushd $HOME/bonediagd
-  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM tmp36 where (sample_time) >=NOW() - INTERVAL 7 DAY;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql21.csv
-  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM temper where (sample_time) >=NOW() - INTERVAL 7 DAY;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql21b.csv
+  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM tmp36 where (sample_time) >=NOW() - INTERVAL 1 DAY;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql21.csv
+  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM temper where (sample_time) >=NOW() - INTERVAL 1 DAY;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql21b.csv
   gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21.gp
 
-  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM dht22 where (sample_time) >=NOW() - INTERVAL 7 DAY;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql22.csv
+  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM dht22 where (sample_time) >=NOW() - INTERVAL 1 DAY;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql22.csv
   gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph22.gp
 
 popd
