c3914f901cddd812062b4fb163751a032d8dd1ac
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-01-30 14:08:48 +0100

    prevent NULL-size graphs

--- testgraph.sh
+++ testgraph.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 30
+
 LOCAL=$(date)
 LOCALSECONDS=$(date -d "$LOCAL" +%s)
 UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
@@ -18,9 +20,12 @@ pushd $HOME/bonediagd
 
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql2c.csv
+
+  touch /tmp/bonediagd/graph.lock
   gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph22.gp
   gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph23.gp
   #gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph24.gp
   gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph25.gp
 
+  rm /tmp/bonediagd/graph.lock
 popd
