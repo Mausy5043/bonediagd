51fb4860be61ea1f79bd8cab01421393465f84a2
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-02-14 17:13:28 +0100

    25 -> 50 hour grouphs

--- testgraph.sh
+++ testgraph.sh
@@ -8,7 +8,7 @@ UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
 UTCSECONDS=$(date -d "$UTC" +%s)
 UTCOFFSET=$(($LOCALSECONDS-$UTCSECONDS))
 
-interval="INTERVAL 25 HOUR "
+interval="INTERVAL 50 HOUR "
 
 pushd $HOME/bonediagd >/dev/null
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM temper where (sample_time) >=NOW() - $interval;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql21.csv
